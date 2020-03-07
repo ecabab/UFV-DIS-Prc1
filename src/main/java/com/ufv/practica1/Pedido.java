9de020395186619e900ca2696c50178fd5eb8386
Gonzalo <gonzalogaraban@hotmail.com>
2020-03-07 23:24:20 +0100

    clase pedido

---
+++ src/main/java/com/ufv/practica1/Pedido.java
@@ -0,0 +1,59 @@
+package com.ufv.practica1;
+
+public class Pedido {
+	private Producto producto;
+	private int cantidad;
+	private Direccion direccion;
+	private String nombre;
+	private String fecha;
+
+	public Pedido(Producto producto, int cantidad, Direccion direccion, String nombre, String fecha) {
+		super();
+		this.producto = producto;
+		this.cantidad = cantidad;
+		this.direccion = direccion;
+		this.nombre = nombre;
+		this.fecha = fecha;
+
+	}
+	public Producto getProducto() {
+		return producto;
+	}
+
+	public void setProducto(Producto producto) {
+		this.producto = producto;
+	}
+
+	public int getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	public Direccion getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(Direccion direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
+}
+
